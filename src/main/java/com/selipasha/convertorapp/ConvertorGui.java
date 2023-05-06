e6549983c4c751381bbf5664b05e8884fefeb795
selipasha <gipwusa@gmail.com>
2023-05-06 11:41:16 +0500

    GUI version almost done

--- src/main/java/com/selipasha/convertorapp/ConvertorGui.java
+++ src/main/java/com/selipasha/convertorapp/ConvertorGui.java
@@ -14,6 +14,7 @@ public class ConvertorGui {
     private JTextField amountTextField;
     private JButton convertButton;
     private JTextField resultTextField;
+    private final Font defaultFont = new Font("Arial", Font.PLAIN, 24);
 
     private static final List<String> ALLOWED_CURRENCIES = Arrays.asList("USD", "KZT", "EUR", "INR", "JPY", "GBP", "AUD", "CAD", "RUB", "MXN", "GEL");
 
@@ -26,19 +27,26 @@ public class ConvertorGui {
     }
 
     private void createUIComponents() {
-        frame = new JFrame("Currency Converter");
+        frame = new JFrame("Конвертер валют");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
 
-        Font defaultFont = new Font("Arial", Font.PLAIN, 24);
-
         fromCurrencyComboBox = new JComboBox<>(ALLOWED_CURRENCIES.toArray(new String[0]));
         fromCurrencyComboBox.setFont(defaultFont);
+        int usdIndex = ALLOWED_CURRENCIES.indexOf("USD");
+        if (usdIndex != -1) {
+            fromCurrencyComboBox.setSelectedIndex(usdIndex);
+        }
+
         toCurrencyComboBox = new JComboBox<>(ALLOWED_CURRENCIES.toArray(new String[0]));
         toCurrencyComboBox.setFont(defaultFont);
+        int rubIndex = ALLOWED_CURRENCIES.indexOf("RUB");
+        if (rubIndex != -1) {
+            toCurrencyComboBox.setSelectedIndex(rubIndex);
+        }
         amountTextField = new JTextField();
         amountTextField.setFont(defaultFont);
-        convertButton = new JButton("Convert");
+        convertButton = new JButton("Конвертировать");
         convertButton.setFont(defaultFont);
         resultTextField = new JTextField();
         resultTextField.setFont(defaultFont);
@@ -52,7 +60,7 @@ public class ConvertorGui {
             try {
                 amount = Integer.parseInt(amountTextField.getText());
             } catch (NumberFormatException ex) {
-                JOptionPane.showMessageDialog(frame, "Invalid amount. Please enter a number.");
+                JOptionPane.showMessageDialog(frame, "Недопустимое значение. Введите число.");
                 return;
             }
 
@@ -66,21 +74,49 @@ public class ConvertorGui {
     }
 
     private void setupUI() {
-        frame.setLayout(new GridLayout(5, 2, 10, 10));
+        frame.setLayout(new GridBagLayout());
         frame.getRootPane().setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        frame.add(new JLabel("From Currency:"));
-        frame.add(fromCurrencyComboBox);
-        frame.add(new JLabel("To Currency:"));
-        frame.add(toCurrencyComboBox);
-        frame.add(new JLabel("Amount:"));
-        frame.add(amountTextField);
-        frame.add(new JLabel("Result:"));
-        frame.add(resultTextField);
-
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.fill = GridBagConstraints.BOTH; // Изменено с HORIZONTAL на BOTH
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.weightx = 1.0; // Добавлено
+        gbc.weighty = 1.0; // Добавлено
+
+        gbc.gridy = 0;
+        JLabel fromCurrencyLabel = new JLabel("Из валюты:");
+        fromCurrencyLabel.setFont(defaultFont);
+        frame.add(fromCurrencyLabel, gbc);
+        gbc.gridx = 1;
+        frame.add(fromCurrencyComboBox, gbc);
+
+        gbc.gridy = 1;
+        gbc.gridx = 0;
+        JLabel toCurrencyLabel = new JLabel("В валюту:");
+        toCurrencyLabel.setFont(defaultFont);
+        frame.add(toCurrencyLabel, gbc);
+        gbc.gridx = 1;
+        frame.add(toCurrencyComboBox, gbc);
+
+        gbc.gridy = 2;
+        gbc.gridx = 0;
+        JLabel amountLabel = new JLabel("Сумма:");
+        amountLabel.setFont(defaultFont);
+        frame.add(amountLabel, gbc);
+        gbc.gridx = 1;
+        frame.add(amountTextField, gbc);
+
+        gbc.gridy = 3;
+        gbc.gridx = 0;
+        JLabel resultLabel = new JLabel("Результат:");
+        resultLabel.setFont(defaultFont);
+        frame.add(resultLabel, gbc);
+        gbc.gridx = 1;
+        frame.add(resultTextField, gbc);
+
+        gbc.gridy = 4;
+        gbc.gridx = 1;
+        gbc.anchor = GridBagConstraints.EAST;
         frame.add(convertButton, gbc);
 
         frame.setLocationRelativeTo(null);
